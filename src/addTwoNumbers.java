452c4504e2d937f73feb3b01f706971cebcd84bb
shirishkirtiwar <shirish.kirtiwar@gmail.com>
2017-06-30 17:43:33 +0530

    ACADGILD Session_1 Assignment_2
    A program to add two numbers without using + operator

---
+++ src/addTwoNumbers.java
@@ -0,0 +1,17 @@
+
+
+public class addTwoNumbers {			//whole program is written inside the class
+	public static void main(String[] args) { //main method means the method is part of its class and not part of objects
+
+		int a;							//initializing variable with the data type integer.
+		int b=56;						//giving values to the variable with data type integer.
+		int c=24;						//	------------------"-----------------------------
+		
+		a= b-(-c);						//operation on a to add numbers without using + operator
+		System.out.println("additon of two numbers is " +a); // printing the output
+	}									//end of block for main method
+	
+
+	
+	
+}										//end of block for addTwoNumbers class
